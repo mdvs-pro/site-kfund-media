2461906ae2383afd53be6cfbf6f8c71b930ddbae
yuchiko <iyuchiko@gmail.com>
2017-04-15 21:54:00 +0300

    change bg style on xs screen, add style to post type

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,11 +1,16 @@
 jQuery(document).ready(function ($) {
 	initSliders();
+	initSearch();
+
+	initSocialSidebar();
+	$('.js__form-send').on('submit', formAjax);
+	
 	var scene = document.getElementById('scene');
 	var parallax = new Parallax(scene);
 });
 
 jQuery(window).load(function () {
-	mobileNav.init('.header__menu');
+	mobileNav.init('.footer-menu');
 });
 
 var mobileNav = {
@@ -58,3 +63,91 @@ function initSliders(){
 	});
 }
 
+function initSearch(){
+	$('.header__search').click( function(){
+	  $('body').addClass('search-active');
+	  $('.input-search').focus();
+	});
+
+	$('.search__close').click( function(){
+	  $('body').removeClass('search-active');
+	});
+}
+
+function InvalidMsg(textbox) {
+
+    if (textbox.value == '') {
+        textbox.setCustomValidity(textbox.getAttribute('title'));
+    }
+    else if(textbox.validity.typeMismatch){
+        textbox.setCustomValidity(textbox.getAttribute('title'));
+    }
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
+
+function customValid() {
+	var _this = event.target;
+	var errorClass = 'form__input--error';
+
+	_this.setCustomValidity(' ');
+	$(_this).addClass(errorClass);
+
+	return true;
+}
+
+function formAjax() {
+	event.preventDefault();
+	console.log('formAjax start');
+	var form = this;
+	var data = $(form).serialize();
+	var pathAction = $(form).attr('action');
+
+	console.log(data);
+  $.ajax({
+    url: pathAction,
+    data: data,
+    type: 'POST',
+    success: function (dataofconfirm) {
+        $(form).siblings('.form__text--success').show('slow');
+        $(form).find('input:not([type="submit"])').each(function(){
+        	$(this).removeClass('form__input--error');
+        	$(this).val('');
+        })
+    }
+  });
+}
+
+function initSocialSidebar() {
+	var SOCIAL_CLASS = '.js-socside';
+	var SOCIAL_ART_CLASS = SOCIAL_CLASS + '-article';
+	var SOCIAL_ACTIVE_CLASS = 'js-socside-show';
+	var SOCIAL_ALWAYS_SHOW = SOCIAL_CLASS + '-always-show';
+	var isAlwaysShow = false;
+
+	if ($(SOCIAL_ALWAYS_SHOW).length) { isAlwaysShow = true; }
+
+	if (!$(SOCIAL_CLASS).length) return;
+
+	// start showing when we see the article
+	var SOCIAL_ART_OBJ = $(SOCIAL_ART_CLASS);
+
+	if (isAlwaysShow) {
+		$(SOCIAL_CLASS).addClass(SOCIAL_ACTIVE_CLASS);
+	} else {
+		window.addEventListener("scroll", function(event) {
+
+		  if (this.scrollY > $(SOCIAL_ART_CLASS).offset().top) {
+		  	if (!$(SOCIAL_CLASS).hasClass(SOCIAL_ACTIVE_CLASS)) {
+		  		$(SOCIAL_CLASS).addClass(SOCIAL_ACTIVE_CLASS);
+		  	}
+		  } else {
+		  	if ($(SOCIAL_CLASS).hasClass(SOCIAL_ACTIVE_CLASS)) {
+		  		$(SOCIAL_CLASS).removeClass(SOCIAL_ACTIVE_CLASS);
+		  	}
+		  }
+		}, false);
+	}
+}
